a794bd264b40020bbc412ab1241cc8301fc35531
IrvanAmri <72341169+IrvanAmri@users.noreply.github.com>
2022-12-08 14:31:53 +0700

    add spring boot initial app

---
+++ src/test/java/com/XtramileSolutions/ApplicationTest/ApplicationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.XtramileSolutions.ApplicationTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
